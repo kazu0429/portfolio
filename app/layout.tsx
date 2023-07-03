ed71bfc37048c7639db4f606ee28ac458b1a5c0e
kazu0429 <75778286+kazu0429@users.noreply.github.com>
2023-07-03 23:13:50 +0900

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className="h-screen w-screen flex justify-center items-center">{children}</body>
     </html>
   )
 }
