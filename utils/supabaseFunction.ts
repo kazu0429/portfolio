1431733f8ad9b158535ca14c8a37ec08e240669a
kazu0429 <k.shouhi0412@i.softbank.jp>
2023-07-06 23:15:29 +0900

    update product降順取得

--- utils/supabaseFunction.ts
+++ utils/supabaseFunction.ts
@@ -1,10 +1,11 @@
 import { supabase } from "./supabase"
 
 export const getProducts = async() => {
-    const products = await supabase.from("products")
+    const {data} = await supabase.from("products")
         .select(`*, images(
             name,
             url
-        )`);
-    return products.data;
+        )`).order('created_at',{ascending:false});
+
+    return data;
 }
