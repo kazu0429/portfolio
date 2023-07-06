998c6ef53c14dfdf68d3abc8729fc7d59d56a2c4
kazu0429 <75778286+kazu0429@users.noreply.github.com>
2023-07-06 23:13:48 +0900

    update login link

--- components/Header.tsx
+++ components/Header.tsx
@@ -26,7 +26,7 @@ const Header = () => {
                         <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-black  hover:text-indigo-500">Portfolio</span>
                     </Link>
                     <div className="flex items-center lg:order-2">
-                        <a href="#" className="px-2 py-1 text-black font-medium text-sm rounded  hover:text-white hover:bg-gray-500 hover:berder-gray-600">Log in</a>
+                        <Link href="/signin" className="px-2 py-1 text-black font-medium text-sm rounded  hover:text-white hover:bg-gray-500 hover:berder-gray-600">Log in</Link>
                         <button onClick={() => setIsOpen(!isOpen)} type="button" data-collapse-toggle="mobile-menu-2" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 hover:bg-white dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
                             <span className="sr-only">Open main menu</span>
                             <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
