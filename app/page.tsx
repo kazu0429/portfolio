3cfed07ca41c2b0c462496fd390b5adb98466ed7
kazu0429 <k.shouhi0412@i.softbank.jp>
2023-07-18 01:01:01 +0900

    rm 未使用のライブラリ

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
-import Image from 'next/image'
-import { cookies } from 'next/headers'
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
-import { Database } from '@/lib/database.types'
 import Home from '@/components/Home'
 
 export default async function App() {
