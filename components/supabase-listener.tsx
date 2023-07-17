fa22d4fb62da982536594dba77f4b7ba93211a9f
kazu0429 <75778286+kazu0429@users.noreply.github.com>
2023-07-18 00:03:17 +0900

    add ログインユーザ状態管理

---
+++ components/supabase-listener.tsx
@@ -0,0 +1,20 @@
+'use server'
+
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
+import { cookies } from "next/headers"
+import type { Database } from "@/lib/database.types"
+import Header from "./Header"
+
+const SupabaseListener = async () => {
+    const supabase = createServerComponentClient<Database>({cookies})
+
+    const {
+        data:{session}
+    } = await supabase.auth.getSession()
+
+    return (
+        <Header session={session}/>
+    )
+}
+
+export default SupabaseListener
