45508250d577ba3300428e00650b2140eb2e7e27
kazu0429 <k.shouhi0412@i.softbank.jp>
2023-07-19 07:19:31 +0900

    change form img to Image

--- components/CardList.tsx
+++ components/CardList.tsx
@@ -1,4 +1,5 @@
 import { Product } from '@/types/product'
+import Image from 'next/image';
 import React from 'react'
 
 
@@ -24,7 +25,7 @@ const CardList = (props: Props) => {
                     {products?.map((product) => (
                         <div key={product.id} className="flex flex-col items-center overflow-hidden rounded-lg border bg-white">
                             <a href="#" className="group relative block overflow-hidden bg-gray-100 w-56 h-48">
-                                <img src={product.images.url} loading="lazy" alt={product.images.name} className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
+                                <Image src={product.images.url} loading="lazy" alt={product.images.name} className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
                             </a>
                             <div className="flex flex-1 flex-col p-4 sm:p-6 lg:p-4 flex-wrap">
                                 <h2 className="mb-2 text-lg font-semibold text-gray-800">
