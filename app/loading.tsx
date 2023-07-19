22b1801a43e71f1bdca61609b053ebd3621480cc
kazu0429 <75778286+kazu0429@users.noreply.github.com>
2023-07-19 14:50:05 +0900

    typo classname

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Loading = () => {
     return (
-        <div className="flex justify-center h-screen" aria-label="読み込み中">
-            <div className="nimate-spin h-10 w-10 border-4 border-indigo-500 rounded-full border-t-transparent"></div>
+        <div className="flex justify-center h-screen items-center" aria-label="読み込み中">
+            <div className="animate-spin h-10 w-10 border-4 border-indigo-500 rounded-full border-t-transparent"></div>
         </div>
     )
 }
