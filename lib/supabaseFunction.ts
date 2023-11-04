46f78a385ec3c14e5a6f38a43ba51a98fcb448f9
kazu0429 <k.shouhi0412@i.softbank.jp>
2023-11-04 13:48:58 +0900

    add get thumbnail func

--- lib/supabaseFunction.ts
+++ lib/supabaseFunction.ts
@@ -1,14 +1,17 @@
 'use server'
 import { supabase } from "./supabase"
-import { cookies } from 'next/headers'
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
-import { Database } from '@/lib/database.types'
 
 export const getProducts = async() => {
-    const {data} = await supabase.from("products")
+    const {data, error} = await supabase.from("products")
         .select(`*, images(
             name,
             url
         )`);
     return data;
+}
+
+export const getThumbnail = async() => {
+    const {data, error} = await supabase.from("images").select(`name`);
+
+    return data
 }
