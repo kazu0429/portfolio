8f89ca6d0fc6f018b315d79e92db91af75d9e16e
kazu0429 <k.shouhi0412@i.softbank.jp>
2023-11-13 13:40:29 +0900

    update postProducts

--- app/api/postProduct/route.ts
+++ app/api/postProduct/route.ts
@@ -1,19 +1,14 @@
 import { supabase } from "@/lib/supabase";
 import { NextResponse, NextRequest } from "next/server";
-import { getProducts } from "@/lib/supabaseFunction";
 
 export async function POST(request: NextRequest) {
     const body = await request.json();
-    
-    const getProductsData = await getProducts();
-    
-    console.log("post data", {id: getProductsData?.length, ...body} );
+
+    console.log("post data", body);
 
     const { data, error } = await supabase
         .from("products")
-        .insert({
-            id: getProductsData?.length, ...body
-        });
+        .insert(body);
 
     if (error) {
         console.log(error);
