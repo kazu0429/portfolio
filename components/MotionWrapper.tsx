ad5a8b22269500b458250796c8b82a3c613e18e2
kazu0429 <k.shouhi0412@i.softbank.jp>
2023-11-16 01:38:40 +0900

    update page motion

--- components/MotionWrapper.tsx
+++ components/MotionWrapper.tsx
@@ -10,17 +10,41 @@ export default function MotionWrapper({
 }) {
     const pathName = usePathname();
 
+    const vatiants = {
+        initial: {
+            height: "100vh",
+            buttom: 0
+        },
+        animate: {
+            height: 0,
+            opacity: 1,
+            transition: {
+                duration: 0.8,
+                ease: [0.87, 0, 0.13, 1]
+            }
+        }
+    }
+
     return (
         <AnimatePresence>
-           <motion.div
-            key={pathName}
-            initial={{ opacity: 0 }} //　初期状態
-            animate={{ opacity: 1 }} // マウント時
-            transition={{duration:0.5 ,delay:0.2}}
-        >
-            {children}
-        </motion.div> 
+            <div className='absolute'>
+                <motion.div
+                    key={pathName}
+                    initial="initial" //　初期状態
+                    animate="animate" // マウント時
+                    variants={vatiants}
+                    transition={{ duration: 0.5, delayChildren: 0.2 }}
+                    className='relative bg-black'
+                >
+                    <motion.div
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        transition={{ duration: 0.5, delay: 0.5 }}
+                    >
+                        {children}
+                    </motion.div>
+                </motion.div>
+            </div>
         </AnimatePresence>
-        
     )
 }
