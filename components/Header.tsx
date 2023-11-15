d3b47f1db3ae45e1667977916e9e910b0806f46c
kazu0429 <k.shouhi0412@i.softbank.jp>
2023-11-16 01:40:55 +0900

    fix indent

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,12 @@
 'use client'
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
-import {Session, createClientComponentClient} from '@supabase/auth-helpers-nextjs'
+import { Session, createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import { Database } from '@/lib/database.types'
 import { useRouter } from 'next/navigation'
 
 
-const Header = ({session}:{session:Session  | null}) => {
+const Header = ({ session }: { session: Session | null }) => {
 
 
     const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -20,23 +20,21 @@ const Header = ({session}:{session:Session  | null}) => {
     ]
 
     const logOut = async () => {
-        const supabase  = createClientComponentClient<Database>()
+        const supabase = createClientComponentClient<Database>()
 
-        try{
+        try {
             const { error } = await supabase.auth.signOut()
             alert("ログアウトしました")
             router.push('/')
-        }catch(err){
+        } catch (err) {
             console.log(err)
-        }finally{
+        } finally {
             router.refresh()
         }
     }
 
-
-
     return (
-        <div className='border-b'>
+        <div className='border-b top-0 w-full'>
             <nav className='border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-white'>
                 <div className='flex flex-wrap justify-between items-center mx-auto max-w-screen-xl'>
                     <Link href="/" className="flex items-center space-x-4">
@@ -46,24 +44,23 @@ const Header = ({session}:{session:Session  | null}) => {
                         </svg>
                         <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-black  hover:text-indigo-500">Portfolio</span>
                     </Link>
-                    <div className="flex items-center lg:order-2">
-                    {session ? (
-                        <button onClick={logOut} className="px-2 py-1 text-black font-medium text-sm rounded  hover:text-white hover:bg-indigo-500 hover:berder-gray-600">Log out</button>
-                    ):(
-                        <Link href='/signin' className="px-2 py-1 text-black font-medium text-sm rounded  hover:text-white hover:bg-indigo-500 hover:berder-gray-600">Log in</Link>
-                    )}
-                    
-                        <button onClick={() => setIsOpen(!isOpen)} type="button" data-collapse-toggle="mobile-menu-2" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 hover:bg-white dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
+                    <div className="flex items-center lg:order-2 md:order-2">
+                        {session ? (
+                            <button onClick={logOut} className="px-2 py-1 text-black font-medium text-sm rounded  hover:text-white hover:bg-indigo-500 hover:berder-gray-600">Log out</button>
+                        ) : (
+                            <Link href='/signin' className="px-2 py-1 text-black font-medium text-sm rounded  hover:text-white hover:bg-indigo-500 hover:berder-gray-600">Log in</Link>
+                        )}
+                        <button onClick={() => setIsOpen(!isOpen)} type="button" data-collapse-toggle="mobile-menu-2" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden md:hidden focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 hover:bg-white dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
                             <span className="sr-only">Open main menu</span>
                             <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
                             <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                         </button>
                     </div>
-                    <div className={`justify-between items-center w-full ${!isOpen && "hidden"} lg:flex lg:w-auto lg:order-1 shadow-black`} id="mobile-menu-2">
-                        <ul className={`flex flex-col lg:flex-row lg:space-x-8 sm:mt-0 shadow-black mt-4 font-medium`}>
+                    <div className={`justify-between items-center lg:flex lg:w-auto md:flex md:w-auto lg:order-1 md:order-1 mt-2 shadow-black bg-white w-full ${!isOpen && "hidden"}`} id="mobile-menu-2">
+                        <ul className={`flex flex-col lg:flex-row lg:space-x-8 md:flex-row md:space-x-8 sm:mt-0 shadow-black mt-4 font-medium`}>
                             {Menus.map((menu, i) => (
                                 <li key={i}>
-                                    <Link href={menu.to} className="block py-2 pr-4 pl-3 hover:text-indigo-500 border-100 lg:border-0 lg:p-0">{menu.name}</Link>
+                                    <Link href={menu.to} className="block py-2 pr-4 pl-3 hover:text-indigo-500 border-100 lg:border-0 lg:p-0 md:p-0">{menu.name}</Link>
                                 </li>
                             ))}
                         </ul>
