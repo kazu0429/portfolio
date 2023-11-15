1ac64341ca1e28585d31077b66de7bec332ec900
kazu0429 <k.shouhi0412@i.softbank.jp>
2023-11-16 01:39:35 +0900

    rm MotionWrapper

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
 import Home from '@/components/Home'
-import MotionWrapper from '@/components/MotionWrapper'
 
 export default async function App() {
 
   return (
-    <MotionWrapper>
-       <Home/>
-    </MotionWrapper>
+    <Home />
   )
 }
