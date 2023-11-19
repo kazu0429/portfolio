14f767a6ece023aee40dcb3d22d6008bba336cf4
kazu0429 <k.shouhi0412@i.softbank.jp>
2023-11-19 22:51:36 +0900

    ad dark mode

--- components/Login.tsx
+++ components/Login.tsx
@@ -32,25 +32,25 @@ const Login = () => {
     }
 
     return (
-        <main className="flex items-center bg-white py-6 sm:py-8 lg:py-12 h-screen">
+        <main className="flex items-center py-6 sm:py-8 lg:py-12 h-screen">
             <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
-                <h2 className="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-8 lg:text-3xl">Sign In!</h2>
-                <form action={signin} className="mx-auto max-w-lg rounded-lg border">
+                <h2 className="mb-4 text-center text-2xl font-boldmd:mb-8 lg:text-3xl">Sign In!</h2>
+                <form action={signin} className="mx-auto max-w-lg rounded-lg border dark:border-4 dark:border-zinc-800">
                     <div className="flex flex-col gap-4 p-4 md:p-8">
                         <div>
-                            <label htmlFor="email" className="mb-2 inline-block text-sm text-gray-800 sm:text-base">Email</label>
-                            <input type="text" name="email" className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring" required />
+                            <label htmlFor="email" className="mb-2 inline-block text-sm  sm:text-base">Email</label>
+                            <input type="text" name="email" className="w-full rounded border bg-gray-5 dark:bg-zinc-800 dark:border-zinc-900 px-3 py-2 text-gray-800 dark:text-white outline-none ring-indigo-300 transition duration-100 focus:ring" required />
                         </div>
                         <div>
-                            <label htmlFor="password" className="mb-2 inline-block text-sm text-gray-800 sm:text-base">Password</label>
-                            <input type="password" name="password" className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring" required />
+                            <label htmlFor="password" className="mb-2 inline-block text-sm  sm:text-base">Password</label>
+                            <input type="password" name="password" className="w-full rounded border  bg-gray-5 dark:bg-zinc-800 dark:border-zinc-900 px-3 py-2 text-gray-800  dark:text-white outline-none ring-indigo-300 transition duration-100 focus:ring" required />
                         </div>
                         <div>
                             <button type="submit" className="w-full mb-2 rounded-lg bg-indigo-500 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-gray-300 transition duration-100 hover:bg-indigo-700 focus-visible:ring active:bg-gray-600 md:text-base">Log in</button>
                         </div>
                     </div>
-                    <div className="flex items-center justify-center bg-gray-100 p-4">
-                        <p className="text-center text-sm text-gray-500">Only administrators can login. <Link as="/" href="/signup" className="text-indigo-500 transition duration-100 hover:text-indigo-600 active:text-indigo-700">Home</Link></p>
+                    <div className="flex items-center justify-center rounded-b-lg bg-gray-100 dark:bg-zinc-600 p-4">
+                        <p className="text-center text-sm text-gray-500 dark:text-white">Only administrators can login. <Link as="/" href="/signup" className="text-indigo-500 dark:text-indigo-300 transition duration-100 hover:text-indigo-600 dark:hover:text-indigo-500 active:text-indigo-700">Home</Link></p>
                     </div>
                 </form>
             </div>
