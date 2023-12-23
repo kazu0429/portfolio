05bbc92adc2d7e224189199de42d5a2fa3e9ae76
kazu0429 <k.shouhi0412@i.softbank.jp>
2023-12-23 22:29:45 +0900

    update skills drag motion

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -5,9 +5,9 @@ import Introduction from './Introduction'
 
 export default function Home() {
     return (
-        <div className='m-5 flex h-auto justify-center items-center'>
-            <div className='m-2 flex lg:flex-row  md:flex-row sm:flex-col lg:justify-center items-center gap-x-12 sm:gap-y-6'>
-                <div className='py-4 px-3 lg:w-1/3 md:w-2/4 sm:w-3/5 flex flex-col bg-gray-100 dark:bg-zinc-800 gap-y-3k'>
+        <div className='h-full m-5'>
+            <div className='m-2 flex-col lg:flex lg:flex-row md:flex md:flex-row sm:flex sm:flex-col justify-center items-center gap-x-12 sm:gap-y-6'>
+                <div className='py-4 px-3 lg:w-1/3 md:w-2/4 sm:w-3/5 flex bg-gray-100 dark:bg-zinc-800 gap-y-3'>
                     <Introduction />
                 </div>
                     <Skills />
--- components/Home/Introduction.tsx
+++ components/Home/Introduction.tsx
@@ -1,9 +1,30 @@
-import { motion } from "framer-motion"
-import Image from "next/image"
+'use client';
+
+import { motion } from "framer-motion";
+import Image from "next/image";
 
 const Introduction = () => {
+
+    const PenSvg = () => {
+        return (
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="flex-none w-4 h-4">
+                <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
+            </svg>
+        )
+    }
+
+    const AcademicSvg = () => {
+        return (
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" data-slot="icon" className="w-4 h-4 text-indigo-500 dark:text-gray-300">
+                <path d="M11.7 2.805a.75.75 0 0 1 .6 0A60.65 60.65 0 0 1 22.83 8.72a.75.75 0 0 1-.231 1.337 49.948 49.948 0 0 0-9.902 3.912l-.003.002c-.114.06-.227.119-.34.18a.75.75 0 0 1-.707 0A50.88 50.88 0 0 0 7.5 12.173v-.224c0-.131.067-.248.172-.311a54.615 54.615 0 0 1 4.653-2.52.75.75 0 0 0-.65-1.352 56.123 56.123 0 0 0-4.78 2.589 1.858 1.858 0 0 0-.859 1.228 49.803 49.803 0 0 0-4.634-1.527.75.75 0 0 1-.231-1.337A60.653 60.653 0 0 1 11.7 2.805Z" />
+                <path d="M13.06 15.473a48.45 48.45 0 0 1 7.666-3.282c.134 1.414.22 2.843.255 4.284a.75.75 0 0 1-.46.711 47.87 47.87 0 0 0-8.105 4.342.75.75 0 0 1-.832 0 47.87 47.87 0 0 0-8.104-4.342.75.75 0 0 1-.461-.71c.035-1.442.121-2.87.255-4.286.921.304 1.83.634 2.726.99v1.27a1.5 1.5 0 0 0-.14 2.508c-.09.38-.222.753-.397 1.11.452.213.901.434 1.346.66a6.727 6.727 0 0 0 .551-1.607 1.5 1.5 0 0 0 .14-2.67v-.645a48.549 48.549 0 0 1 3.44 1.667 2.25 2.25 0 0 0 2.12 0Z" />
+                <path d="M4.462 19.462c.42-.419.753-.89 1-1.395.453.214.902.435 1.347.662a6.742 6.742 0 0 1-1.286 1.794.75.75 0 0 1-1.06-1.06Z" />
+            </svg>
+        )
+    }
+
     return (
-        <div className="h-[630px] sm:h-[635px] md:h-[640px] lg:h-[650px] xl:h-[660px] overflow-auto">
+        <div className="scroll-content h-[600px] sm:h-[630px] md:h-[640px] lg:h-[650px] xl:h-[660px] overflow-y-scroll overscroll-y-auto">
             <div>
                 <motion.div
                     initial={{ scale: 0 }}
@@ -49,44 +70,43 @@ const Introduction = () => {
                 </div>
             </div>
             <div>
-                <h2 className="mb-4 text-2xl font-bold">Educational</h2>
+                <h2 className="mt-4 mb-4 text-2xl font-bold">Educational</h2>
                 <ol className="ml-5 relative border-s border-gray-400 dark:border-gray-600">
                     <li className="mb-10 ms-6">
                         <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-indigo-200 dark:ring-gray-900 dark:bg-indigo-800">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" data-slot="icon" className="w-4 h-4 text-indigo-500 dark:text-gray-300">
-                            <path d="M11.7 2.805a.75.75 0 0 1 .6 0A60.65 60.65 0 0 1 22.83 8.72a.75.75 0 0 1-.231 1.337 49.948 49.948 0 0 0-9.902 3.912l-.003.002c-.114.06-.227.119-.34.18a.75.75 0 0 1-.707 0A50.88 50.88 0 0 0 7.5 12.173v-.224c0-.131.067-.248.172-.311a54.615 54.615 0 0 1 4.653-2.52.75.75 0 0 0-.65-1.352 56.123 56.123 0 0 0-4.78 2.589 1.858 1.858 0 0 0-.859 1.228 49.803 49.803 0 0 0-4.634-1.527.75.75 0 0 1-.231-1.337A60.653 60.653 0 0 1 11.7 2.805Z" />
-                            <path d="M13.06 15.473a48.45 48.45 0 0 1 7.666-3.282c.134 1.414.22 2.843.255 4.284a.75.75 0 0 1-.46.711 47.87 47.87 0 0 0-8.105 4.342.75.75 0 0 1-.832 0 47.87 47.87 0 0 0-8.104-4.342.75.75 0 0 1-.461-.71c.035-1.442.121-2.87.255-4.286.921.304 1.83.634 2.726.99v1.27a1.5 1.5 0 0 0-.14 2.508c-.09.38-.222.753-.397 1.11.452.213.901.434 1.346.66a6.727 6.727 0 0 0 .551-1.607 1.5 1.5 0 0 0 .14-2.67v-.645a48.549 48.549 0 0 1 3.44 1.667 2.25 2.25 0 0 0 2.12 0Z" />
-                            <path d="M4.462 19.462c.42-.419.753-.89 1-1.395.453.214.902.435 1.347.662a6.742 6.742 0 0 1-1.286 1.794.75.75 0 0 1-1.06-1.06Z" />
-                        </svg>
+                            <AcademicSvg/>
                         </span>
                         <time className="text-sm mb-1 font-normal leading-none text-gray-400 dark:text-gray-500">2017/4 - 2020/3</time>
                         <p className="font-bold">小倉商業高校</p>
                         <p className="text-sm">ビジネス情報学科</p>
+                        <div className="mt-1 flex items-baseline gap-x-2 text-gray-500 dark:text-gray-300">
+                            <PenSvg />
+                            <p>簿記/プログラミング(Java)/ネットワーク</p>
+                        </div>
                     </li>
                     <li className="mb-10 ms-6">
                         <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-indigo-200 dark:ring-gray-900 dark:bg-indigo-800">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" data-slot="icon" className="w-4 h-4 text-indigo-500 dark:text-gray-300">
-                            <path d="M11.7 2.805a.75.75 0 0 1 .6 0A60.65 60.65 0 0 1 22.83 8.72a.75.75 0 0 1-.231 1.337 49.948 49.948 0 0 0-9.902 3.912l-.003.002c-.114.06-.227.119-.34.18a.75.75 0 0 1-.707 0A50.88 50.88 0 0 0 7.5 12.173v-.224c0-.131.067-.248.172-.311a54.615 54.615 0 0 1 4.653-2.52.75.75 0 0 0-.65-1.352 56.123 56.123 0 0 0-4.78 2.589 1.858 1.858 0 0 0-.859 1.228 49.803 49.803 0 0 0-4.634-1.527.75.75 0 0 1-.231-1.337A60.653 60.653 0 0 1 11.7 2.805Z" />
-                            <path d="M13.06 15.473a48.45 48.45 0 0 1 7.666-3.282c.134 1.414.22 2.843.255 4.284a.75.75 0 0 1-.46.711 47.87 47.87 0 0 0-8.105 4.342.75.75 0 0 1-.832 0 47.87 47.87 0 0 0-8.104-4.342.75.75 0 0 1-.461-.71c.035-1.442.121-2.87.255-4.286.921.304 1.83.634 2.726.99v1.27a1.5 1.5 0 0 0-.14 2.508c-.09.38-.222.753-.397 1.11.452.213.901.434 1.346.66a6.727 6.727 0 0 0 .551-1.607 1.5 1.5 0 0 0 .14-2.67v-.645a48.549 48.549 0 0 1 3.44 1.667 2.25 2.25 0 0 0 2.12 0Z" />
-                            <path d="M4.462 19.462c.42-.419.753-.89 1-1.395.453.214.902.435 1.347.662a6.742 6.742 0 0 1-1.286 1.794.75.75 0 0 1-1.06-1.06Z" />
-                        </svg>
+                            <AcademicSvg/>
                         </span>
                         <time className="text-sm mb-1 font-normal leading-none text-gray-400 dark:text-gray-500">2020/4 - 2022/3</time>
                         <p className="font-bold">福岡工業大学短期大学部</p>
                         <p className="text-sm">情報工学コース</p>
+                        <div className="mt-1 flex flex-row justify-center items-baseline gap-x-2 text-gray-500 dark:text-gray-300">
+                            <PenSvg />
+                            <p>プログラミング(C・Java・PHP・Python)/ネットワーク・サーバー/NLP</p>
+                        </div>
                     </li>
                     <li className="mb-10 ms-6">
                         <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-indigo-200 dark:ring-gray-900 dark:bg-indigo-800">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" data-slot="icon" className="w-4 h-4 text-indigo-500 dark:text-gray-300">
-                            <path d="M11.7 2.805a.75.75 0 0 1 .6 0A60.65 60.65 0 0 1 22.83 8.72a.75.75 0 0 1-.231 1.337 49.948 49.948 0 0 0-9.902 3.912l-.003.002c-.114.06-.227.119-.34.18a.75.75 0 0 1-.707 0A50.88 50.88 0 0 0 7.5 12.173v-.224c0-.131.067-.248.172-.311a54.615 54.615 0 0 1 4.653-2.52.75.75 0 0 0-.65-1.352 56.123 56.123 0 0 0-4.78 2.589 1.858 1.858 0 0 0-.859 1.228 49.803 49.803 0 0 0-4.634-1.527.75.75 0 0 1-.231-1.337A60.653 60.653 0 0 1 11.7 2.805Z" />
-                            <path d="M13.06 15.473a48.45 48.45 0 0 1 7.666-3.282c.134 1.414.22 2.843.255 4.284a.75.75 0 0 1-.46.711 47.87 47.87 0 0 0-8.105 4.342.75.75 0 0 1-.832 0 47.87 47.87 0 0 0-8.104-4.342.75.75 0 0 1-.461-.71c.035-1.442.121-2.87.255-4.286.921.304 1.83.634 2.726.99v1.27a1.5 1.5 0 0 0-.14 2.508c-.09.38-.222.753-.397 1.11.452.213.901.434 1.346.66a6.727 6.727 0 0 0 .551-1.607 1.5 1.5 0 0 0 .14-2.67v-.645a48.549 48.549 0 0 1 3.44 1.667 2.25 2.25 0 0 0 2.12 0Z" />
-                            <path d="M4.462 19.462c.42-.419.753-.89 1-1.395.453.214.902.435 1.347.662a6.742 6.742 0 0 1-1.286 1.794.75.75 0 0 1-1.06-1.06Z" />
-                        </svg>
+                            <AcademicSvg/>
                         </span>
                         <time className="text-sm mb-1 font-normal leading-none text-gray-400 dark:text-gray-500">2022/4 ~ 現在</time>
                         <p className="font-bold">鹿児島大学</p>
                         <p className="text-sm">工学部 先進工学科 情報・生体工学PG</p>
-                        <span>機械学習</span>
+                        <div className="mt-1 flex items-start gap-x-2 text-gray-500 dark:text-gray-300">
+                            <PenSvg />
+                            <p>画像処理/機械学習/最適化/ネットワーク/</p>
+                        </div>
                     </li>
                 </ol>
             </div>
