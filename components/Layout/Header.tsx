18fed747ccbf7c2597a720f62083a46edc2b3033
kazu0429 <k.shouhi0412@i.softbank.jp>
2023-12-28 02:04:36 +0900

    Update Header Icon

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -1,6 +1,9 @@
 'use client'
+
 import React, { useState } from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
+
 import { Session, createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import { Database } from '@/lib/database.types'
 import { useRouter } from 'next/navigation'
@@ -39,11 +42,8 @@ const Header = ({ session }: { session: Session | null }) => {
             <nav className='border-gray-200 px-4 lg:px-6 md:px-6 py-2.5 bg-opacity-80 backdrop-blur-sm bg-white dark:bg-opacity-80 dark:bg-black dark:border-black'>
                 <div className='flex justify-between items-center mx-auto max-w-screen-xl'>
                     <Link href="/" className="flex items-center space-x-4">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="mr-3 w-8 h-8  hover:text-indigo-500">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M15.362 5.214A8.252 8.252 0 0112 21 8.25 8.25 0 016.038 7.048 8.287 8.287 0 009 9.6a8.983 8.983 0 013.361-6.867 8.21 8.21 0 003 2.48z" />
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 18a3.75 3.75 0 00.495-7.467 5.99 5.99 0 00-1.925 3.546 5.974 5.974 0 01-2.133-1A3.75 3.75 0 0012 18z" />
-                        </svg>
-                        <span className="self-center text-xl font-semibold whitespace-nowrap  hover:text-indigo-500">Portfolio</span>
+                        <Image src="/image/icon-resize.png" height={35} width={35} alt='profile' className="rounded-2xl hover:scale-110" />
+                        <span className="self-center text-xl font-semibold whitespace-nowrap  hover:text-indigo-300">KazuPortfolio</span>
                     </Link>
                     <div className="flex items-center lg:order-2 md:order-2">
                         <div className='inline-flex items-center'>
